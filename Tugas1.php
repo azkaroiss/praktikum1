d6aada395c661edafbd311d3ac8e4319e877278f
Azka Rois Sya'bana <azka.roiss@gmail.com>
2025-09-27 19:14:27 +0700

    second commmit

--- Tugas1.php
+++ Tugas1.php
@@ -4,4 +4,11 @@ echo "JSON Encode:<br>";
 echo json_encode($data);
 echo "<hr>";
 
-$json = '{"nama":"Azka","umur",24}';
+$json = '{"nama":"Azka","umur":24}';
+
+$obj = json_decode($json);
+echo "Object: " . $obj->nama . " - " . $obj->umur . "<br>";
+
+$arr = json_decode($json, true);
+echo "Array: " . $arr["nama"] . " - " . $arr["umur"];
+?>
